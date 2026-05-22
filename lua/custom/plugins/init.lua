2eeb96b3e99f7f604caa49ac5951e153d4d076d7
Jerry <jerry@Jerrys-Mac-mini.local>
2026-05-22 08:18:27 +0800

    feat: installed mark down plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,20 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreview', 'MarkdownPreviewStop', 'MarkdownPreviewToggle' },
+    ft = 'markdown',
+    build = 'cd app && bash install.sh',
+    init = function()
+      vim.g.mkdp_open_to_the_world = 1
+      vim.g.mkdp_open_ip = '0.0.0.0'
+      vim.g.mkdp_port = '8090'
+      vim.g.mkdp_echo_preview_url = 1
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_browser = 'echo'
+      vim.api.nvim_create_user_command('Md', 'MarkdownPreview', {})
+    end,
+  },
+}
